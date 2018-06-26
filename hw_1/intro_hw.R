58ff03ea415c045e5f73e9a5f2bedc099974a6a9
Greg Harvey <gregsharvey@gmail.com>
2018-06-26 09:51:41 -0400

    Add files via upload

---
+++ hw_1/intro_hw.R
@@ -0,0 +1,98 @@
+
+### Intro to R ### 
+
+# Question 1.
+# Using R as a calculator, what is the sum of 15 and 32 save as a variable named answer1?  
+
+answer1 <- 
+
+# Question 2.
+# Using R as a calculator, what is the product of 34 and 543 save as answer2?  
+
+answer2 <- 
+
+# Question 3.
+# Using R as a calculator, what is the value of 2 raised to the 4th?  
+  
+answer3 <- 
+
+# Question 4.
+# Create a vector of the numbers 3,8,4,2 and give it the name A.
+
+A <- 
+
+# Question 5.
+# Create a vector of the characters “monkey”, “rat”, “snake”, and give it the name B.
+
+B <- 
+  
+# Question 6.
+# Create a vector of the sequence of numbers between 89 and 143, and give it the name C.  
+
+C <- 
+  
+# Question 7.
+# Create a vector containing 15 of the number 3 – use the rep command, and give it the name D.  
+  
+D <- 
+  
+# Question 8
+# What is the 3rd element in the vector A use list indexing?  
+
+answer8 <- 
+
+# Question 9
+# What are the last two elements of the vector B use list indexing?  
+
+answer9 <- 
+
+# Question 10
+# What is the 4th and 30th element in the vector C return them as a list of both elements?
+
+answer10 <- 
+
+# Question 11
+# What is the sum of all of the elements in the vector D? 
+
+answer11 <- 
+
+# Question 12
+# Add the character “bird” to the end of vector B.
+
+B2 <- 
+
+# Question 13
+# Change the first element in vector D to the number 5.
+
+D2 <- 
+
+# Question 14
+# Multiple vector A by 3 and save the results as vector A2.  
+
+A2 <- 
+
+# Question 15
+# Add 5 to every element of vector C and save the results as C2. 
+
+C2 <- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hw_1/test_intro_hw.R
@@ -0,0 +1,26 @@
+source("./intro_hw/intro_hw.R")
+library(testthat)
+
+D2_answer <- D
+D2_answer[1] <- 5
+
+test_that("hw answers", {
+  
+  expect_equal(answer1, 15+32)
+  expect_equal(answer2, 34*543)
+  expect_equal(answer3, 2^4)
+  expect_equal(A, c(3,8,4,2))
+  expect_equal(B, c("monkey","rat","snake"))
+  expect_equal(C, seq(89,143))
+  expect_equal(D, rep(3, 15))
+  expect_equal(answer8, A[3])
+  expect_equal(answer9, B[2:3])
+  expect_equal(answer10, C[c(4,30)])
+  expect_equal(answer11, sum(D))
+  expect_equal(B2, c(B, "bird"))
+  expect_equal(D2, D2_answer)
+  expect_equal(A2, A*3)
+  expect_equal(C2, C+5)
+  
+})
+
