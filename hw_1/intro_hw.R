d851d2ddc3e7a2e6b6413ecc8d2bba7538603478
Greg Harvey <gregsharvey@gmail.com>
2018-07-26 09:49:45 -0400

    Update intro_hw.R

--- hw_1/intro_hw.R
+++ hw_1/intro_hw.R
@@ -4,77 +4,77 @@
 # Question 1.
 # Using R as a calculator, what is the sum of 15 and 32 save as a variable named answer1?  
 
-answer1 <- 
+answer1 <- 15+32
 
 # Question 2.
 # Using R as a calculator, what is the product of 34 and 543 save as answer2?  
 
-answer2 <- 
+answer2 <- 34*543
 
 # Question 3.
 # Using R as a calculator, what is the value of 2 raised to the 4th?  
   
-answer3 <- 
+answer3 <- 2^4
 
 # Question 4.
 # Create a vector of the numbers 3,8,4,2 and give it the name A.
 
-A <- 
+A <- c(3,8,4,2)
 
 # Question 5.
 # Create a vector of the characters “monkey”, “rat”, “snake”, and give it the name B.
 
-B <- 
+B <- c("monkey","rat","snake")
   
 # Question 6.
 # Create a vector of the sequence of numbers between 89 and 143, and give it the name C.  
 
-C <- 
+C <- seq(89,143)
   
 # Question 7.
 # Create a vector containing 15 of the number 3 – use the rep command, and give it the name D.  
   
-D <- 
+D <- rep(3, 15)
   
 # Question 8
 # What is the 3rd element in the vector A use list indexing?  
 
-answer8 <- 
+answer8 <- A[3]
 
 # Question 9
 # What are the last two elements of the vector B use list indexing?  
 
-answer9 <- 
+answer9 <- B[2:3]
 
 # Question 10
 # What is the 4th and 30th element in the vector C return them as a list of both elements?
 
-answer10 <- 
+answer10 <- C[c(4,30)]
 
 # Question 11
 # What is the sum of all of the elements in the vector D? 
 
-answer11 <- 
+answer11 <- sum(D)
 
 # Question 12
 # Add the character “bird” to the end of vector B.
 
-B2 <- 
+B2 <- c(B, "bird")
 
 # Question 13
 # Change the first element in vector D to the number 5.
 
-D2 <- 
+D2 <- D[1] <- 5
 
 # Question 14
 # Multiple vector A by 3 and save the results as vector A2.  
 
-A2 <- 
+A2 <- A*3
 
 # Question 15
 # Add 5 to every element of vector C and save the results as C2. 
 
-C2 <- 
+C2 <- C+5
 
 
 
